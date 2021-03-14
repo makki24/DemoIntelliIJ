50d83e6209333b951de9ccefb53e6eb0a4058d06
makki24 <56727072+makki24@users.noreply.github.com>
2021-03-14 19:57:04 +0530

    Update HelloServlet.java

--- src/main/java/com/example/demo/HelloServlet.java
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -11,7 +11,7 @@ public class HelloServlet extends HttpServlet
 
     public void init()
     {
-        message = "Hello World!";
+        message = "Hello World makki!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
